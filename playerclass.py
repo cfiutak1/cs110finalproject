daf5a55956422dd37ebdeb69b3a23102365ac8f4
cfiutak1 <cfiutak1@binghamton.edu>
2016-12-02 02:46:38 -0500

    Player class (Very incomplete)

---
+++ playerclass.py
@@ -0,0 +1,24 @@
+import random
+
+class Player:
+	def __init__(self, players):
+		#For debug
+		print(len(players))
+
+		self.players = players
+
+		def checkBustOut(self,players):
+			if self.player1stash == 0:
+				return True
+			else:
+				return False
+
+		def player1(self, name, stash, cards, AIflag):
+			self.stash = stash
+			
+
+
+def main():
+	startplayers = ["player1", "player2", "player3", "player4", "player5"]
+	Player(startplayers)
+main()
